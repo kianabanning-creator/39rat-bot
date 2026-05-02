853051d9a6c667238e9b737e8bc0b6760e1485a4
kianabanning-creator <kianabanning@gmail.com>
2026-05-02 19:23:02 -0400

    ao_rat_bot.py

--- ao_rat_bot.py
+++ ao_rat_bot.py
@@ -1,7 +1,8 @@
 import discord
 from discord.ext import commands
+import os
 
-TOKEN = "YOUR_BOT_TOKEN_HERE"
+TOKEN = os.environ.get("TOKEN") 
 
 intents = discord.Intents.default()
 intents.message_content = True
